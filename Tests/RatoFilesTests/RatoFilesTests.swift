851d971494b8f40faabf5223fda19d672f114f0b
sayzim28-pixel <sayzim28@gmail.com>
2026-09-07 19:05:39 -0300

    Delete Tests/RatoFilesTests directory

--- Tests/RatoFilesTests/RatoFilesTests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-@testable import RatoFilesCore
-
-final class RatoFilesTests: XCTestCase {
-    func testSandboxPath() {
-        let home = PermissionsManager.shared.getSandboxHomeDirectory()
-        XCTAssertFalse(home.path.isEmpty)
-    }
-}
